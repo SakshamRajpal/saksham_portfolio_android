d007e58f73fe9888bb68b9177c6eb0ceea540314
SakshamRajpal <110560344+SakshamRajpal@users.noreply.github.com>
2025-05-08 13:43:12 +0530

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,7 +1,6 @@
 import React, { Suspense, useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Decal, OrbitControls, Preload, useTexture } from "@react-three/drei";
-
 import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
@@ -18,7 +17,7 @@ const Ball = (props) => {
           metalness={0.1}
           clearcoat={0.9}
           clearcoatRoughness={0.1}
-        />
+        /> 
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.2]}
@@ -41,15 +40,15 @@ const BallCanvas = ({ icon }) => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
+      style={{ width: "100%", height: "100%" }} // Ensure canvas fits container
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} enableRotate={false} />
+        <OrbitControls enableZoom={false} enableRotate={!isMobile} /> {/* Enable rotation only on desktop */}
         <Ball imgUrl={icon} />
       </Suspense>
-
       <Preload all />
     </Canvas>
   );
